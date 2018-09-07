4a9928753d3accf6dc26e3748d0af96e4cdd58c5
Alejandro Soto <asoto@gabinete.mx>
2018-09-06 23:57:03 -0500

    inicio de funcionalidad de Calculadora:
    * Tecla presionada reduzca su tamaño y vuelva a su forma original al soltarla
    
    Finalizado

--- js/app.js
+++ js/app.js
@@ -1,16 +1,3 @@
-// Variables 
-/*
-function boton_press(elemento){
-  //elemento.style.width = "18%";
-  console.log(elemento,"press")
-}
-function boton_up(elemento){
-  //elemento.style.width = "20%";
-  console.log(elemento,"up")
-}
-
-*/
-
 
 // Clase Calculadora
 var Calculadora = {
@@ -22,6 +9,7 @@ var Calculadora = {
 		sessionStorage.setItem('Operaciones', JSON.stringify(this.Operaciones))
 	},
 	iniciar_cero: function(){
+		// Funcion inicial de display en 0
 		var display = document.getElementById("display")
 		var display_val = display.innerHTML
 		if (display_val > 0){
@@ -29,10 +17,12 @@ var Calculadora = {
 		}
 	},
 	asignarEventosBotones: function(selector){
+		// Funcion inicial de asignacion de eventos en la clase tecla
 	    var botonesPagina = document.getElementsByClassName(selector);
 	    for (var i = 0; i < botonesPagina.length; i++) {
 	      botonesPagina[i].onclick = this.eventoPressBotonAccion;
-	      botonesPagina[i].onkeyup = this.eventUpBotonAccion;
+	      botonesPagina[i].onmouseleave = this.teclaUp
+	      botonesPagina[i]["down"] = this.teclaDown
 	      botonesPagina[i]["calcula"] = this.calculaOper
 	      botonesPagina[i]["ordena"] = this.ordenOper
 	    }
@@ -43,14 +33,15 @@ var Calculadora = {
 		var tecla_val = JSON.parse(sessionStorage.getItem('Cantidades'))
 		var display = document.getElementById("display")
 		
-		//tecla.style.width = "18%";
+		// Boton mas pequeño
+		self.down(event)
 
 		// Tecla On de reinicio
 		if(tecla.alt == "On"){
 			Calculadora.init()
 		}
 
-		//Teclas numericas
+		//Teclas numericas y simbolos
 		if(tecla.alt>=0){
 			if(tecla_val.length <=7){
 				tecla_val += tecla.alt
@@ -99,10 +90,28 @@ var Calculadora = {
 			display.innerHTML = "0"
 		}
 	},
-	eventUpBotonAccion(event){
-		this.boton_up(event.target)
+	teclaUp(event){
+		var tecla = event.target
+		if(tecla.alt != "mas"){		
+			tecla.style.height = "62.91px";
+	    	tecla.style.width = "76.98px";
+		}else{
+			tecla.style.height = "149.55px";
+	    	tecla.style.width = "78px";
+		}
+	},
+	teclaDown(event){
+		var tecla = event.target
+		if(tecla.alt != "mas"){
+			tecla.style.height = "59.91px";
+	    	tecla.style.width = "75.98px";
+		}else{
+			tecla.style.height = "147.56px";
+	    	tecla.style.width = "75px";
+		}
 	},
 	ordenOper: function(cantidad,oper){
+		//Funcion de ordenamiento de operaciones
 		var Operaciones = JSON.parse(sessionStorage.getItem('Operaciones'))
 		if (oper!="="){
 			Operaciones["Oper"] = oper
@@ -113,21 +122,19 @@ var Calculadora = {
 			}else{
 				Operaciones["P1"] = parseFloat(cantidad)
 			}
-		}else if(Operaciones.P2==""){
+		}else{
 			if(cantidad.indexOf(".") == -1){
 				Operaciones["P2"] = parseInt(cantidad)
 			}else{
 				Operaciones["P2"] = parseFloat(cantidad)
 			}
-		}else{
-			console.log("aqui 2 -- repite",oper,Operaciones)
 		}
 		sessionStorage.setItem('Operaciones', JSON.stringify(Operaciones))
-		console.log("aqui",oper,Operaciones)
+		//console.log("aqui",oper,Operaciones)
 	},
 	calculaOper: function(){
+		// Funcion de calculo aritmetico de operaciones
 		var Operaciones = JSON.parse(sessionStorage.getItem('Operaciones'))
-		console.log(Operaciones)
 		var resultados = ""
 		if (Operaciones["Oper"] =="+"){
 			resultados = Operaciones["P1"] + Operaciones["P2"]
@@ -140,7 +147,7 @@ var Calculadora = {
 		}else{
 			display.innerHTML = "calculando"
 		}
-		Operaciones["P1"] = resultados 
+		Operaciones["P1"] = "" 
 		sessionStorage.setItem('Operaciones', JSON.stringify(Operaciones))
 		sessionStorage.setItem('Cantidades', JSON.stringify(String(resultados)))
 
@@ -152,4 +159,5 @@ var Calculadora = {
 	}
 }
 
+// Inicio del modulo calculadora
 Calculadora.init();
